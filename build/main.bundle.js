24407897cf2c00753d2fb083f0284ff61a5ec1be
DEGGY_MUDI <iyinevb@icloud.com>
2017-10-18 13:15:01 +0100

    commit changes

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -1,9 +1,11 @@
-let calculateMonthlyPayment = function (principal, years, rate) {
-    let monthlyRate = 0;
+'use strict';
+
+var calculateMonthlyPayment = function calculateMonthlyPayment(principal, years, rate) {
+    var monthlyRate = 0;
     if (rate) {
-        monthlyRate = (rate / 100) / 12;
+        monthlyRate = rate / 100 / 12;
     }
-    let monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
+    var monthlyPayment = principal * monthlyRate / (1 - Math.pow(1 / (1 + monthlyRate), years * 12));
     return monthlyPayment;
 };
 
